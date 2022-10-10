6a808291f8d0b44ad16f4ae5171e9c6573d0502b
DonToli <strt87@yandex.ru>
2022-10-10 07:16:15 +0300

    Leet Code add RichestCustomerWealth

---
+++ src/LeetCode/richestCustomerWealth/RichestCustomerWealth.java
@@ -0,0 +1,15 @@
+package LeetCode.richestCustomerWealth;
+
+public class RichestCustomerWealth {
+    public int maximumWealth(int[][] accounts) {
+        int res = 0;
+        for (int[] customer : accounts) {
+            int sum = 0;
+            for (int account : customer) {
+                sum += account;
+            }
+            res = Math.max(res, sum);
+        }
+        return res;
+    }
+}
