5b0d37e412a711e153072aa63d80d65b4623a275
DonToli <strt87@yandex.ru>
2022-10-11 23:57:48 +0300

    Leet Code add RansomNote

---
+++ src/LeetCode/ransomNote/RansomNote.java
@@ -0,0 +1,15 @@
+package LeetCode.ransomNote;
+
+import java.util.HashMap;
+
+public class RansomNote {
+    public boolean canConstruct(String ransomNote,String magazine){
+        HashMap<Character,Integer> counts = new HashMap<>();
+        for (char)
+
+
+
+
+        return true;
+    }
+}
